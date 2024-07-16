3e90c22a676a81fafcf5ef4613542256b4c074c2
Bilshansakib <burakaghani@gmail.com>
2024-07-16 15:29:08 +0600

    Update: Routes- register,login, dashboard

--- client/src/Auth/Register.jsx
+++ client/src/Auth/Register.jsx
@@ -1,21 +1,52 @@
-import { Card,Flex, Typography } from "antd";
+import { Card, Flex, Form, Input, message, Typography } from "antd";
 
 const Register = () => {
-    return (
-        <Card className="flex items-center justify-start border-2 ">
-            <Flex>
-                <Flex>
-                    {/* form */}
-                    <Typography.Title level={3} strong className="title">
-                        Create an account
-                    </Typography.Title>
-                    <Typography.Text type="secondary" level={3} strong className="title">
-                        Join for excluesive access
-                    </Typography.Text>
-                </Flex>
-            </Flex>
-        </Card>
-    );
+  const handleRegister = (values) => {
+    console.log(values);
+  };
+  return (
+    <Card className="h-screen w-[1000px] flex items-center justify-start border-2 ">
+      <Flex>
+        <Flex vertical flex={1}>
+          {/* form */}
+          <Typography.Title level={3} strong className="title">
+            Create an account
+          </Typography.Title>
+          <Typography.Text type="secondary" level={3} strong className="title">
+            Join for excluesive access
+          </Typography.Text>
+        </Flex>
+        <Form layout="vertical" onFinish={handleRegister} autoComplete="off">
+          <Form.Item
+            label="Full Name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: "please input your full name",
+              },
+            ]}
+          >
+            <Input placeholder="Enter your full name"></Input>
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "please input your full email",
+              },
+            ]}
+          >
+            <Input placeholder="Enter your full name"></Input>
+          </Form.Item>
+        </Form>
+
+        {/* Image */}
+      </Flex>
+    </Card>
+  );
 };
 
 export default Register;
