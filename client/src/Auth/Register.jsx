6536597427fbdb6c15e222dd294b8cd1a1d6cea9
Bilshansakib <burakaghani@gmail.com>
2024-07-16 16:13:49 +0600

    Update: register form

--- client/src/Auth/Register.jsx
+++ client/src/Auth/Register.jsx
@@ -1,4 +1,6 @@
-import { Card, Flex, Form, Input, message, Typography } from "antd";
+import { Button, Card, Flex, Form, Input, message, Typography } from "antd";
+import { Link } from "react-router-dom";
+import  registerImage  from "../assets/MFSAppRegPage.jpg";
 
 const Register = () => {
   const handleRegister = (values) => {
@@ -15,33 +17,93 @@ const Register = () => {
           <Typography.Text type="secondary" level={3} strong className="title">
             Join for excluesive access
           </Typography.Text>
+          <Form layout="vertical" onFinish={handleRegister} autoComplete="off">
+            <Form.Item
+              label="Full Name"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: "please input your full name",
+                },
+              ]}
+            >
+              <Input size="large" placeholder="Enter your full name"></Input>
+            </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              size="large"
+              rules={[
+                {
+                  required: true,
+                  message: "please input your email",
+                },
+                {
+                  type: "email",
+                  message: "Invalid email, Sweet Tit for Sweet Tat",
+                },
+              ]}
+            >
+              <Input size="large" placeholder="Enter your email"></Input>
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              size="large"
+              rules={[
+                {
+                  required: true,
+                  message: "please input your password",
+                },
+              ]}
+            >
+              <Input.Password
+                size="large"
+                placeholder="Enter your password"
+              ></Input.Password>
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="passwordConfirm"
+              size="large"
+              rules={[
+                {
+                  required: true,
+                  message: "please input your Confirm password",
+                },
+              ]}
+            >
+              <Input.Password
+                size="large"
+                placeholder="Re-Enter your password"
+              ></Input.Password>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                size="large"
+                className="btn w-full"
+              >
+                Create Account
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <Link to="/login">
+                <Button size="large" className="btn btn-link w-full">
+                  {" "}
+                  Sign In
+                </Button>
+              </Link>
+            </Form.Item>
+          </Form>
         </Flex>
-        <Form layout="vertical" onFinish={handleRegister} autoComplete="off">
-          <Form.Item
-            label="Full Name"
-            name="name"
-            rules={[
-              {
-                required: true,
-                message: "please input your full name",
-              },
-            ]}
-          >
-            <Input placeholder="Enter your full name"></Input>
-          </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: "please input your full email",
-              },
-            ]}
-          >
-            <Input placeholder="Enter your full name"></Input>
-          </Form.Item>
-        </Form>
+        <Flex flex={1}>
+          <img src={registerImage} className="w-full"></img>
+        </Flex>
 
         {/* Image */}
       </Flex>
